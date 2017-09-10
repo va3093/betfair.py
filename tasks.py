7b5440c5ac16c8d872d70433786c2c7170215774
Wilhelm <va3093@gmail.com>
2017-09-10 14:41:23 +0100

    fixed invoke scripts

--- tasks.py
+++ tasks.py
@@ -8,22 +8,22 @@ DEFAULT_BITS = 2048
 
 
 @task
-def generate_key(name=DEFAULT_NAME, bits=DEFAULT_BITS):
+def generate_key(ctx, name=DEFAULT_NAME, bits=DEFAULT_BITS):
     key_file = '{0}.key'.format(name)
     cmd = 'openssl genrsa -out {0} {1}'.format(key_file, bits)
-    run(cmd)
+    ctx.run(cmd)
 
 
 @task
-def generate_cert(name=DEFAULT_NAME):
+def generate_cert(ctx, name=DEFAULT_NAME):
     key_file = '{0}.key'.format(name)
     csr_file = '{0}.csr'.format(name)
     cmd = 'openssl req -new -key {0} -out {1}'.format(key_file, csr_file)
-    run(cmd)
+    ctx.run(cmd)
 
 
 @task
-def sign_cert(name=DEFAULT_NAME):
+def sign_cert(ctx, name=DEFAULT_NAME):
     key_file = '{0}.key'.format(name)
     csr_file = '{0}.csr'.format(name)
     crt_file = '{0}.crt'.format(name)
@@ -32,21 +32,21 @@ def sign_cert(name=DEFAULT_NAME):
         csr_file,
         crt_file,
     )
-    run(cmd)
+    ctx.run(cmd)
 
 
 @task
-def generate_pem(name=DEFAULT_NAME):
+def generate_pem(ctx, name=DEFAULT_NAME):
     key_file = '{0}.key'.format(name)
     crt_file = '{0}.crt'.format(name)
     pem_file = '{0}.pem'.format(name)
     cmd = 'cat {0} {1} > {2}'.format(crt_file, key_file, pem_file)
-    run(cmd)
+    ctx.run(cmd)
 
 
 @task
-def ssl(name=DEFAULT_NAME, bits=DEFAULT_BITS):
-    generate_key(name, bits)
-    generate_cert(name)
-    sign_cert(name)
-    generate_pem(name)
+def ssl(ctx, name=DEFAULT_NAME, bits=DEFAULT_BITS):
+    generate_key(ctx,name, bits)
+    generate_cert(ctx, name)
+    sign_cert(ctx, name)
+    generate_pem(ctx, name)
